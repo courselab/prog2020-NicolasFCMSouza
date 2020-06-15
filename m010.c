1baef1a281614a86394e5941c555b5e51d472db2
NicolasFCMSouza <nicolasfcmsouza@usp.br>
2020-06-15 22:49:04 +0000

    correção

--- m010.c
+++ m010.c
@@ -28,8 +28,7 @@ enum {sun, mon, tue, wed, thu, fri, sat};
 /* Return the day of the week in the numeric form:
    Sunday=0, Monday=1... Saturday=6.*/
 
-int day_of_week (int day, int month)
-{
+int day_of_week (int day, int month){
   int i = 0;
   int ddm[12];
   int soma = 0;
